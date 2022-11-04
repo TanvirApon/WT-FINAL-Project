9049fcc11679e16f50e387766e255a6bf8abb352
TanvirApon <71035839+TanvirApon@users.noreply.github.com>
2022-11-04 23:12:47 +0600

    Delete View directory

--- View/Seller/Footer.php
+++
@@ -1,1 +0,0 @@
-<p style="text-align: center;"> Copyright @<?php echo date("Y") ?></p>
--- View/Seller/practice.php
+++
@@ -1,7 +0,0 @@
-<table>
-	<tr>
-		<td style="text-align: center;"> <h1>Online Shop Management System</h1>
-		</td>
-	</tr>
-
-</table>
