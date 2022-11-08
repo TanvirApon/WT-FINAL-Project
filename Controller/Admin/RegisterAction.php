60e83abc071a76ab200dd0a906a347f18d605027
tanvirhossainapon <tanvirhossainapon@gmail.com>
2022-11-08 09:33:52 +0600

    Some Updates made

--- Controller/Admin/RegisterAction.php
+++ Controller/Admin/RegisterAction.php
@@ -1,43 +1,155 @@
 <?php 
-if($_SERVER['REQUEST_METHOD']==="POST")
-{
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$cpassword = $_POST['cpassword'];
-$ques = $_POST['question'];
-header("Location:../../View/Admin/Admin.php");
 
-}
-$filename = "../../Model/admin.json";
-$file= fopen($filename, "r");
-$data="";
-if(filesize($filename) > 0)
-{
-	$data= fread($file, filesize($filename));
+		$fname=$lname=$email=$password=$cpassword=$ques="";
+		$fnameEr=$lnameEr=$emailEr=$passwordEr=$cpasswordEr=$quesEr='';
+		$flag=false;
+		$successMesg = $errorMesg = "";
+		
+		if(htmlspecialchars($_SERVER['REQUEST_METHOD']=="POST"))
+		{
+			$fname = $_POST['fname'];
+                        $lname = $_POST['lname'];
+                        $email = $_POST['email'];
+                        $password = $_POST['password'];
+                        $cpassword = $_POST['cpassword'];
+                        $ques = $_POST['question'];
+			
+                        header("Location:../../View/Admin/Admin.php");
+			
+                        if(empty($fname))
+			{
+				$fnameEr="First Name is required  ";
+				$flag=true;
+				
+			}
+			else
+			{
+				$fname=senitize($fname);
+				$flag=false;
+			}
 
-}
-fclose($file);
-
-$file=fopen($filename,"w");
-
-if(empty($data))
-{
-	$content =array(array("FirstName" => "$fname" , "LastName" => "$lname","Email" => "$email","Password" => "$password","Answer"=>"$ques"));
-	$content = json_encode($content);
-	fwrite($file, $content);
-
-}
-else
-{
-	$data= json_decode($data);
-	$new_content =array("FirstName" => "$fname","LastName" => "$lname","Email" => "$email","Password" => "$password","Answer"=>"$ques");
+                        if(empty($lname))
+			{
+				$lnameEr="Last name is required  ";
+				$flag=true;
+				
+			}
+			else
+			{
+				$lname=senitize($lname);
+				$flag=false;
+			}
 
 
-	$data[]=$new_content;
-	$data= json_encode($data);
-	fwrite($file, $data);
+			if(empty($email))
+			{
+				$emailEr="Email is required  ";
+				$flag=true;
+				
+			}
+			else
+			{
+				$email=senitize($email);
+				$flag=false;
+			}
+			if(empty($password))
+			{
+				$passwordEr= "Password is  required  ";
+				$flag=true;
+				
+			}
+			else
+			{
+				$password=senitize($password);
+				$flag=false;
+			}
+			if(empty($cpassword))
+			{
+				$cpasswordEr= "Confirm password is  required  ";
+				$flag=true;
+				
+			}
+			else if($cpassword!==$password)
+			{
+				$cpasswordEr= "Confirm password is  not matched  ";
+				$flag=true;
 
-}
+			}
+			else
+			{
+				$cpassword=senitize($cpassword);
+				$flag=false;
+			}
+                         
+                        if(empty($ques))
+			{
+				$quesEr="Answer is required  ";
+				$flag=true;
+				
+			}
+			else
+			{
+				$qus=senitize($ques);
+				$flag=false;
+			}
+
+			
+
+			if(!$flag)
+			{
+				$filename="../../Model/admin.json";
+				$mode="a";
+				$handle=fopen($filename, $mode);
+
+				$arr1=array(
+							
+							"FirstName" => "$fname" , "LastName" => "$lname","Email" => "$email","Password" => "$password","Answer"=>"$ques"
+						);
+
+				$encode=json_encode($arr1);
+				$current_data = file_get_contents($filename);  
+				$array_data = json_decode($current_data, true);  //array
+
+				$arr2=array(
+						
+							"FirstName" => "$fname" , "LastName" => "$lname","Email" => "$email","Password" => "$password","Answer"=>"$ques"
+						);
+
+				$array_data[]=$arr2;
+
+				$final_encode=json_encode($array_data);
+
+				fwrite($handle, $final_encode);
+
+				$read_data=file_put_contents('../../Model/admin.json', $final_encode);
+                                
+                                
+
+				if($read_data)
+				{
+					$successMesg="save successfully";
+                                     sheader("Location: ../../View/Admin/Admin.php");
+
+				}
+				else
+				{
+					$errorMesg="error while saving";
+				}
+
+				
+			}
+
+
+			
+	
+		}
+		function senitize($data)
+		{
+			$data=trim($data);
+			$data=stripcslashes($data);
+			$data=htmlspecialchars($data);
+			return $data;
+
+		}
+		
 ?>
